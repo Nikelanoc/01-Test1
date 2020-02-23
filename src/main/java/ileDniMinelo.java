739eaa7e0ea553c0bde2ea1c3a9a04a19b91e989
Nikola <nikola.nocon@gmail.com>
2020-02-23 13:21:49 +0100

    Added day, month, year

--- src/main/java/ileDniMinelo.java
+++ src/main/java/ileDniMinelo.java
@@ -1,11 +1,19 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class ileDniMinelo {
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       Scanner miesiac = miesiac;
-       Scanner rok = rok;
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+        {
+            Scanner scanner = new Scanner(System.in);
+            Scanner dzien = int i;
+            int i = 23;
+            Scanner miesiac = int a;
+            int a = int luty;
+            Scanner rok = int b;
+            int b = 2020;
 
-
+        }
     }
 }
